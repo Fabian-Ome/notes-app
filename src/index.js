69765aac9fe2d132b688b2a569a160d48121c37b
Fabian-Ome <fabiantabla@hotmail.com>
2021-11-22 20:26:14 -0500

    Notes-App

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ const methodOverride = require('method-override');
 const session = require('express-session');
 const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
 const Handlebars = require('handlebars');
+const flash = require('connect-flash');
+const passport = require('passport');
 
 //Initializations
 const app = express();
 require('./database');
+require('./config/passport');
 
-//Settings 
+
+
+//Settings
 app.set('port', process.env.port || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({
@@ -30,8 +35,18 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 //Global Variables
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    next();
+});
 
 //Routes
 app.use(require('./routes/index'));
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const Note = require('../models/Note');
+const { isAuthenticated } = require('../helpers/auth');
 
-router.get('/notes/add', (req, res) => {
+router.get('/notes/add', isAuthenticated, (req, res) => {
     res.render('notes/new-note');
 })
 
-router.post('/notes/new-note', async (req, res) => {
+router.post('/notes/new-note', isAuthenticated, async (req, res) => {
     const { title, description } = req.body;
     const errors = [];
     if (!title) {
@@ -24,21 +25,35 @@ router.post('/notes/new-note', async (req, res) => {
     }
     else {
         const newNote = new Note({ title, description });
+        newNote.user = req.user.id;
         await newNote.save();
+        req.flash('success_msg', 'Note added Successfully');
         res.redirect('/notes');
     }
 })
 
-router.get('/notes', async (req, res) => {
-    const notes = await Note.find().sort({ date: 'desc' });
+router.get('/notes', isAuthenticated, async (req, res) => {
+    const notes = await Note.find({ user: req.user.id }).sort({ date: 'desc' });
     res.render('notes/all-notes', { notes });
-    console.log(notes);
 });
 
-router.get('/notes/edit/:id', async (req, res) => {
+router.get('/notes/edit/:id', isAuthenticated, async (req, res) => {
     const note = await Note.findById(req.params.id);
     res.render('notes/edit-note', { note });
 
+});
+
+router.put('/notes/edit-note/:id', isAuthenticated, async (req, res) => {
+    const { title, description } = req.body;
+    await Note.findByIdAndUpdate(req.params.id, { title, description })
+    req.flash('success_msg', 'Note Updated Successfully');
+    res.redirect('/notes');
+});
+
+router.delete('/notes/delete/:id', isAuthenticated, async (req, res) => {
+    req.flash('success_msg', 'Note deleted Successfully');
+    await Note.findByIdAndDelete(req.params.id);
+    res.redirect('/notes');
 });
 
 module.exports = router;
